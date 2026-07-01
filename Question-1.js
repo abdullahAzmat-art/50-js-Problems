322b8b63f62970ff0657c01a41a4cca8db8aa28f
Abdullah <abdullah@gmail.com>
2026-07-01 12:29:29 +0500

    Question no 1 done

---
+++ Question-1.js
@@ -0,0 +1,16 @@
+// Check if a number is even or odd
+
+
+var checkfunction = (n)=>{
+
+    if (n % 2==0) {
+        return "even ";
+    }else{
+       return "odd";
+    }
+}
+
+console.log(checkfunction(2))
+console.log(checkfunction(5))
+console.log(checkfunction(6))
+console.log(checkfunction(21))
