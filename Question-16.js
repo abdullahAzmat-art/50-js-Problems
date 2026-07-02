293fdc385ff9c5c70729f8e895c98bb5050c924b
Abdullah <abdullah@gmail.com>
2026-07-02 16:38:15 +0500

    Question no 16 done

---
+++ Question-16.js
@@ -0,0 +1,14 @@
+function countoccurance(str){
+
+    let obj = {}
+    for (let i = 0; i < str.length; i++) {
+        if(obj[str[i]]) {
+            obj[str[i]]++
+        } else {
+            obj[str[i]] = 1
+        }
+    }
+    return obj
+}
+
+        console.log(countoccurance("hello world"))
