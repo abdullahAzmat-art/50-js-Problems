5572708640f94719d8c1aa5a8c5d3f1236763258
Abdullah <abdullah@gmail.com>
2026-07-07 12:10:49 +0500

    Question-30.js done

---
+++ Question-30.js
@@ -0,0 +1,15 @@
+function moveZero(arr) {
+    let j = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== 0) {
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+            j++;
+        }
+    }
+
+    return arr;
+}
+
+let arr = [0, 1, 0, 3, 7];
+console.log(moveZero(arr));
