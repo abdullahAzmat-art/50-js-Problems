3e2b66fd0805afcb0f6eda0e8d52263e786c6121
Abdullah <abdullah@gmail.com>
2026-07-11 22:44:10 +0500

    Question 35 done

---
+++ Question-35.js
@@ -0,0 +1,15 @@
+function checkwords(str){
+let obj = {};
+for (let i = 0; i < str.length; i++) {
+    if (!obj[str[i]]) {
+        obj[str[i]] = 1
+    }else{
+        obj[str[i]] +=1
+    }
+    
+}
+return obj
+     
+}
+
+console.log(checkwords("jello"))
